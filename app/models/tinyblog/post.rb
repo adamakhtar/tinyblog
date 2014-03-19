10dc7003a8da8eb8fb7d67578a536b15cebaa1fe
robodisco <adamsubscribe@googlemail.com>
2014-03-19 11:50:49 +0900

    show post preview in index

--- app/models/tinyblog/post.rb
+++ app/models/tinyblog/post.rb
@@ -1,5 +1,15 @@
 module Tinyblog
   class Post < ActiveRecord::Base
+    
+    include ::ActionView::Helpers::TextHelper
+
     belongs_to :author
+
+    #
+    # Short preview of post's body for use in the index.
+    #
+    def preview
+      truncate(body, length: Tinyblog.post_preview_length, separator: "\n")
+    end
   end
 end
--- lib/tinyblog.rb
+++ lib/tinyblog.rb
@@ -1,6 +1,16 @@
 require "tinyblog/engine"
 
 module Tinyblog
-  mattr_accessor :disqus_shortname, :twitter_username, :social_buttons_on
+  mattr_accessor :disqus_shortname, 
+                 :twitter_username, 
+                 :social_buttons_on,
+                 :post_preview_length
+
+
+  class << self
+    def post_preview_length
+      @@post_preview_length || 400
+    end
+  end
 
 end
