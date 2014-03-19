5727d5fc63badc5f123e13e65f50e845db2aaeca
robodisco <adamsubscribe@googlemail.com>
2014-03-19 14:54:18 +0900

    route author under admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,12 @@
 Tinyblog::Engine.routes.draw do
 
-  get "authors/index"
-  get "authors/new"
-  get "authors/create"
-  get "authors/edit"
-  get "authors/update"
-  get "authors/destroy"
   root :to => 'posts#index'
 
   resources :posts, :only => [:index, :show]
 
   namespace :admin do 
     root :to => 'posts#index'
-    
+    resources :authors
     resources :posts
     resources :authors
     resources :pictures, :only => [:create]
