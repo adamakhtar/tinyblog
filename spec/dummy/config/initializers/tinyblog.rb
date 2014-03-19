0c3eace9bd23baa7cbc4c5337ef40583fdff83d1
robodisco <adamsubscribe@googlemail.com>
2014-03-19 17:02:23 +0900

    delete dummy files which i added via initiliazers

--- spec/dummy/config/initializers/tinyblog.rb
+++
@@ -1,7 +0,0 @@
-
-# Enable to have social share buttons for your posts. - make sure to also specify your twitter user name below.
-Tinyblog.share_buttons_on = true
-
-# Your twitter handle (without the @) - essential if you have enabled share_buttons_on
-Tinyblog.twitter_username = 'robodisco'
-
