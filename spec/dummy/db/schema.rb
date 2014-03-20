fa19f90784324bc17ae5d49dad836a1740313747
robodisco <adamsubscribe@googlemail.com>
2014-03-20 15:42:20 +0900

    add published_at attribute

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140320033901) do
+ActiveRecord::Schema.define(version: 20140320062305) do
 
   create_table "tinyblog_authors", force: true do |t|
     t.string   "first_name"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140320033901) do
     t.datetime "updated_at"
     t.integer  "author_id"
     t.text     "meta_description"
+    t.datetime "published_at"
   end
 
   create_table "users", force: true do |t|
--- spec/models/tinyblog/post_spec.rb
+++ spec/models/tinyblog/post_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 module Tinyblog
   describe Post do
-    
+    it "sets published_at on save" do
+      create(:post).published_at.should be_kind_of Time
+    end
   end
 end
