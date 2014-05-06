c1bb8da497a5c6f4c663b24c1232347493e58e83
robodisco <adamsubscribe@googlemail.com>
2014-05-06 21:14:43 +0900

    oops allow markdown to render links

--- app/helpers/tinyblog/posts_helper.rb
+++ app/helpers/tinyblog/posts_helper.rb
@@ -1,7 +1,7 @@
 module Tinyblog
   module PostsHelper
     def render_content(content)
-      renderer = Redcarpet::Render::HTML.new(no_links: true, hard_wrap: true)
+      renderer = Redcarpet::Render::HTML.new(hard_wrap: true)
       markdown = Redcarpet::Markdown.new(renderer, extensions = {})
       markdown.render(content).html_safe
     end
