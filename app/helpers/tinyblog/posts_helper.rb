c8bebb72b89357a90f63e82583ffd9f4918e588a
robodisco <adamsubscribe@googlemail.com>
2014-08-12 17:13:43 +0900

    enable fragment caching

--- app/helpers/tinyblog/posts_helper.rb
+++ app/helpers/tinyblog/posts_helper.rb
@@ -5,5 +5,11 @@ module Tinyblog
       markdown = Redcarpet::Markdown.new(renderer, extensions = {})
       markdown.render(content).html_safe
     end
+
+    def cache_key_for_posts
+      count          = Post.count
+      max_updated_at = Post.maximum(:updated_at).try(:utc).try(:to_s, :number)
+      "posts/all-#{count}-#{max_updated_at}"
+    end
   end
 end
